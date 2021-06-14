f2fa5c19fbb0611e312bcc6d52a4b53472a52b7f
alex.tylor <atylor@gmail.com>
2021-06-14 23:01:07 +0100

    fix nvim-tree icons, remove homegrown lua filetypes as built in now

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -2,4 +2,5 @@ local opts = {noremap = true}
 local function keymap(...) vim.api.nvim_buf_set_keymap(0, ...) end
 
 -- Run all tests
-keymap("n", "<leader>gt", ":TestRunAll<CR>", opts)
+-- TODO this isn't working?!?
+keymap("n", "<leader>gtf", "<Plug>PlenaryTestFile", opts)
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -17,36 +17,37 @@ function M.toggle_nvim()
 end
 
 function M.setup()
+  -- These need to be set before the plugin in loaded
+  vim.g.nvim_tree_show_icons = {
+    git = 0,
+    folders = 1,
+    icons = 0
+  }
+
+  vim.g.nvim_tree_icons = {
+    folder = {
+      default =    "ᐅ",
+      open =       "ᐁ",
+      empty =      "ᐅ",
+      empty_open = "ᐅ",
+      symlink =    "ᐅ",
+    }
+  }
+  vim.g.nvim_tree_auto_close = 1
+  vim.g.nvim_tree_follow = 1
+  vim.g.nvim_tree_quit_on_open = 1
+  vim.g.nvim_tree_indent_markers = 1
+  vim.g.nvim_tree_disable_window_picker = 1
+  -- vim.g.nvim_tree_width_allow_resize = 1
+
   plug.add({
     url = "kyazdani42/nvim-tree.lua",
     post_handler  = function()
-
       create_mappings({
         n = {
           ["<c-n>"] = "<cmd>lua require('plugins.nvim-tree').toggle_nvim()<CR>",
         }
       })
-
-      vim.g.nvim_tree_show_icons = {
-        git = 0,
-        folders = 1,
-        icons = 0
-      }
-
-      vim.g.nvim_tree_icons = {
-        folder = {
-          default =    "ᐅ",
-          open =       "ᐁ",
-          empty =      "ᐅ",
-          empty_open = "ᐅ",
-          symlink =    "ᐅ",
-        }
-      }
-      vim.g.nvim_tree_auto_close = 1
-      vim.g.nvim_tree_follow = 1
-      vim.g.nvim_tree_quit_on_open = 1
-      vim.g.nvim_tree_indent_markers = 1
-      -- vim.g.nvim_tree_width_allow_resize = 1
     end
   })
 end
