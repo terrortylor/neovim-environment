7e38b5993b72b5828a0123975c98acb5b2d6e2c6
alex.tylor <alex.tylor@e2x.co.uk>
2021-09-30 17:46:34 +0100

    remove code to pull in snippets from json files

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,14 +4,8 @@ local function setup_snippets()
   local ls = require"luasnip"
 
   local s = ls.snippet
-  -- local sn = ls.snippet_node
-  -- local isn = ls.indent_snippet_node
   local parse_snippet = ls.parser.parse_snippet
   local t = ls.text_node
-  -- local i = ls.insert_node
-  -- local f = ls.function_node
-  -- local c = ls.choice_node
-  -- local d = ls.dynamic_node
 
   ls.snippets = {
     all = {
@@ -44,22 +38,6 @@ func Test$1(t *testing.T) {
       ]]),
     }
   }
-
-  local function get_snippets_rtp()
-    return vim.tbl_map(function(itm)
-      return vim.fn.fnamemodify(itm, ":h")
-    end, vim.api.nvim_get_runtime_file(
-    "package.json",
-    true
-    ))
-  end
-
-
-  -- Adds custom snippets in vscode format over top of friendly-snippets
-  -- but doesn't override them, whihc would be better
-  local paths = get_snippets_rtp()
-  table.insert(paths, "./snippets")
-  require("luasnip.loaders.from_vscode").load({paths = paths})
 end
 
 function  M.setup()
