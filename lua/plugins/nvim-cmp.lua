6ae2731f3510367a43b30a7f4e37892d486adfba
alex.tylor <alex.tylor@e2x.co.uk>
2021-11-04 15:42:44 +0000

    don't select automatically

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -104,7 +104,7 @@ function M.setup()
 
       ['<CR>'] = cmp.mapping.confirm({
         behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
+        select = false,
       }),
 --       ["<Tab>"] = cmp.mapping(function(fallback)
 --         if cmp.visible() then
