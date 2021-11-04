01722181dee0da0b1eb14d8c2e765927da019f37
alex.tylor <alex.tylor@e2x.co.uk>
2021-11-04 15:43:10 +0000

    func to run command and move back to original location after

--- lua/util/buffer.lua
+++ lua/util/buffer.lua
@@ -6,9 +6,16 @@ function M.get_all_lines(buf)
 end
 
 -- TODO add tests
+-- TODO is this still required? is it not in the api?
 function M.get_buf_id(file)
   local buf = api.nvim_call_function("bufnr", {file})
   return buf
+end
+
+function M.run_and_back_to_mark(command)
+  local mark = vim.api.nvim_win_get_cursor(0)
+  vim.cmd(command)
+  vim.api.nvim_win_set_cursor(0, mark)
 end
 
 return M
