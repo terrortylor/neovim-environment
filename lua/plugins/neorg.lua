f1459745ae3eb827efdb02ee5e24454fba953261
alex.tylor <alex.tylor@e2x.co.uk>
2022-01-26 22:55:54 +0000

    FEATURE setup some better neorg gtg capture view global mappings

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -66,3 +66,16 @@ require('neorg').setup {
     end)
   end
 }
+
+
+  vim.api.nvim_add_user_command("GtdCapture", function(params)
+    require('neorg').org_file_entered(true, "silent=true")
+    vim.cmd("Neorg gtd capture")
+  end, {force = true})
+  vim.api.nvim_set_keymap("n", "<leader>otc", "<CMD>GtdCapture<CR>", {noremap = true})
+
+  vim.api.nvim_add_user_command("GtdView", function(params)
+    require('neorg').org_file_entered(true, "silent=true")
+    vim.cmd("Neorg gtd views")
+  end, {force = true})
+  vim.api.nvim_set_keymap("n", "<leader>otv", "<CMD>GtdView<CR>", {noremap = true})
