6cdf19b1c292681d66d0f7e43ed13161038e6611
alex.tylor <alex.tylor@e2x.co.uk>
2022-03-24 22:22:20 +0000

    Remove cw > ciw mapping, doesn't work

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -104,7 +104,6 @@ require("util.config").create_mappings({
       { noremap = true, silent = true, expr = true },
     },
     ["<leader>/"] = [[/\<\><left><left>]],
-    ["cw"] = "ciw",
   },
   v = {
     -- Indent Lines and reselect
