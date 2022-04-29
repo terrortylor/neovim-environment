d2b8f95aed4cfa5fbe32316bf6ae92c8cc110e37
alex.tylor <atylor@gmail.com>
2022-04-29 21:29:36 +0100

    auto save notes .... let's see if this is annoying

--- lua/config/autogroups.lua
+++ lua/config/autogroups.lua
@@ -1,4 +1,11 @@
-local ag = vim.api.nvim_create_augroup("cursor_line_group", { clear = true })
+local ag = vim.api.nvim_create_augroup("push_notes", { clear = true })
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = "*/personal-workspace/notes/*",
+  command = "!~/personal-workspace/notes/push.sh",
+  group = ag,
+})
+
+ag = vim.api.nvim_create_augroup("cursor_line_group", { clear = true })
 vim.api.nvim_create_autocmd("WinEnter", {
   pattern = "*",
   command = "setlocal cursorline",
