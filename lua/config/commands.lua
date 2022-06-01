e379f7ff657ef64bd73b821192d6364b32913b0b
alex.tylor <atylor@gmail.com>
2022-06-01 13:38:18 +0100

    remove gron, do not use it any more

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -21,14 +21,3 @@ user_command("Mkdir", "lua require('util.filesystem').mkdir(<args>)", {
 })
 user_command("SetCWDToBuffer", "cd %:p:h")
 user_command("IntelliJ", "lua require('config.intellij-mappings')")
-
--- Gron transforms JSON to 'discrete assignments to make it easier to grep'
--- essentially flatterns in. Woth noting that it doesn't presrve original order but
--- useful to for exploring a json file.
-if vim.api.nvim_call_function("executable", { "gron" }) > 0 then
-  -- gron
-  user_command("Gron", "%!gron")
-  user_command("UnGron", "%!gron --ungron")
-else
-  log.error("gron not found on command line, no Gron commands created")
-end
