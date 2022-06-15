7ab422ac931a07909849c1caa88870deff50b859
alex.tylor <atylor@gmail.com>
2022-06-15 23:02:46 +0100

    update some neorg mappings

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -4,15 +4,15 @@ require("neorg").setup({
     ["core.keybinds"] = { -- Configure core.keybinds
       config = {
         neorg_leader = "<Leader>o", -- This is the default if unspecified
-        hook = function(keybinds)
-          -- luacheck: ignore
-          keybinds.remap_event(
-            "core.integrations.telescope.insert_file_link",
-            "i",
-            "<c-f>",
-            "core.integrations.telescope.insert_file_link"
-          )
-        end,
+        -- hook = function(keybinds)
+        --   -- luacheck: ignore
+        --   keybinds.remap_event(
+        --     "core.integrations.telescope.insert_file_link",
+        --     "i",
+        --     "<c-f>",
+        --     "core.integrations.telescope.insert_file_link"
+        --   )
+        -- end,
       },
     },
     ["core.norg.concealer"] = {
@@ -51,6 +51,26 @@ require("neorg").setup({
     ["core.norg.journal"] = {},
   },
 })
+
+local neorg_callbacks = require("neorg.callbacks")
+
+neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
+    -- Map all the below keybinds only when the "norg" mode is active
+    keybinds.map_event_to_mode("norg", {
+        -- n = { -- Bind keys in normal mode
+        --     { "<C-s>", "core.integrations.telescope.find_linkable" },
+        --     { "<C-f>", "core.integrations.telescope.insert_file_link" },
+        -- },
+
+        i = { -- Bind in insert mode
+            { "<C-l>", "core.integrations.telescope.insert_link" },
+            { "<C-f>", "core.integrations.telescope.insert_file_link" },
+        },
+    }, {
+        silent = true,
+        noremap = true,
+    })
+end)
 
 vim.api.nvim_create_user_command("GtdCapture", function(_)
   require("neorg").org_file_entered(true, "silent=true")
