0eafecb0aeb5b6b2db4996648586efa16eacb93f
alex.tylor <atylor@gmail.com>
2022-07-19 21:07:00 +0100

    fat finger escape

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -109,6 +109,7 @@ set("n", "gp", "`[v`]", {desc = "Reselect last put"})
     set("n", "gy", 'gv"+y', {desc = "select last selection and yank to system clipboard"})
 
     set("i", "jj", "<ESC>", {desc = "Exit insert mode"})
+    set("i", "kk", "<ESC>", {desc = "Exit insert mode"})
 
     set("i", "<PageUp>", "", {desc = "Disbale pagedown in insert mode, keep hitting by mistake"})
     set("i", "<PageDown>", "", {desc = "Disbale pageup in insert mode, keep hitting by mistake"})
