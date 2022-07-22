5d65ce02b36eaa0c8577f216f084ae39baa6e734
alex.tylor <atylor@gmail.com>
2022-07-22 17:46:46 +0100

    comment to scratch search for

--- lua/scratch/search_for.lua
+++ lua/scratch/search_for.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.rename()
   local cword = vim.fn.expand("<cWORD>")
+  -- TODO replace spaces iwth + and make web safe
   os.execute('xdg-open https://www.google.com/search?q=' .. cword)
 end
 
