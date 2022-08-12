4c5b99e21628e8d3b595013039f08864befca3f8
alex.tylor <atylor@gmail.com>
2022-08-12 11:25:42 +0100

    fix nvim-surround deprecated warning

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,63 +1,37 @@
 require("nvim-surround").setup({
-delimiters = {
-  pairs = {
-    [")"] = { "( ", " )" },
-    ["("] = { "(", ")" },
-    ["}"] = { "{ ", " }" },
-    ["{"] = { "{", "}" },
-    [">"] = { "< ", " >" },
-    ["<"] = { "<", ">" },
-    ["]"] = { "[ ", " ]" },
-    ["["] = { "[", "]" },
-    ["*"] = { "*", "*" },
-    ["/"] = { "/", "/" },
-    ["-"] = { "-", "-" },
-    ["_"] = { "_", "_" },
-    ["|"] = { "|", "|" },
-    ["^"] = { "^", "^" },
-    ["$"] = { "$", "$" },
-    ["="] = { "=", "=" },
-    ["+"] = { "+", "+" },
-    -- Define pairs based on function evaluations!
-    ["i"] = function()
-      return {
-        require("nvim-surround.utils").get_input(
-        "Enter the left delimiter: "
-        ),
-        require("nvim-surround.utils").get_input(
-        "Enter the right delimiter: "
-        )
-      }
-    end,
-    ["f"] = function()
-      return {
-        require("nvim-surround.utils").get_input(
-        "Enter the function name: "
-        ) .. "(",
-        ")"
-      }
-    end,
+  surrounds = {
+    [")"] = {
+      add = { "( ", " )" },
+    },
+    ["("] = {
+      add = { "(", ")" },
+    },
+    ["}"] = {
+      add = { "{ ", " }" },
+    },
+    ["{"] = {
+      add = { "{", "}" },
+    },
+    [">"] = {
+      add = { "< ", " >" },
+    },
+    ["<"] = {
+      add = { "<", ">" },
+    },
+    ["]"] = {
+      add = { "[ ", " ]" },
+    },
+    ["["] = {
+      add = { "[", "]" },
+    },
   },
-  separators = {
-    ["'"] = { "'", "'" },
-    ['"'] = { '"', '"' },
-    ["`"] = { "`", "`" },
+  aliases = {
+    ["a"] = "<", -- Single character aliases apply everywhere
+    ["b"] = "(",
+    ["B"] = "{",
+    ["r"] = "[",
+    -- Table aliases only apply for changes/deletions
+    ["q"] = { '"', "'", "`" }, -- Any quote character
+    ["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
   },
-  HTML = { -- Disables HTML-style mappings
-  ["t"] = "type", -- Change just the tag type
-  ["T"] = "whole", -- Change the whole tag contents
-},
-aliases = {
-  ["a"] = "<", -- Single character aliases apply everywhere
-  ["b"] = "(",
-  ["B"] = "{",
-  ["r"] = "[",
-  -- Table aliases only apply for changes/deletions
-  ["q"] = { '"', "'", "`" }, -- Any quote character
-  ["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
-},
-      },
-      highlight_motion = { -- Highlight before inserting/changing surrounds
-      duration = 0,
-    }
-  })
+})
