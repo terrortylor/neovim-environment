66bfbe2827293b0e2fad479ae2718cbc82dda5d1
alex.tylor <atylor@gmail.com>
2022-09-06 15:14:25 +0100

    feature: don't spell check norg urls

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -10,3 +10,7 @@ set("n", "<leader>ff", ":Telescope neorg find_linkable<CR>", {buffer = true})
 -- add hook to nvim tree rename
 -- TODO move to setup call in module, and add filetype check
 require("neorgtools").nvimTreeRenameEventHook()
+
+-- Don't spell check URL's
+-- https://vi.stackexchange.com/questions/3990/ignore-urls-and-email-addresses-in-spell-file
+-- vim.cmd("syn match UrlNoSpell '\w\+:%/\/[^[:space:]]\+' contains=@NoSpell")
