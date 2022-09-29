53e92d2a0803a00c648c1a3b933bd11b8a793d42
alex.tylor <atylor@gmail.com>
2022-09-29 10:01:52 +0100

    chore: neorg mappigs cleanup

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -5,15 +5,17 @@ require("neorg").setup({
     ["core.keybinds"] = { -- Configure core.keybinds
       config = {
         neorg_leader = "<Leader>o", -- This is the default if unspecified
-        -- hook = function(keybinds)
-        --   -- luacheck: ignore
-        --   keybinds.remap_event(
-        --     "core.integrations.telescope.insert_file_link",
-        --     "i",
-        --     "<c-f>",
-        --     "core.integrations.telescope.insert_file_link"
-        --   )
-        -- end,
+        hook = function(keybinds)
+          -- luacheck: ignore
+          keybinds.remap_event( "norg", "i",
+            "<c-f>",
+            "core.integrations.telescope.insert_file_link"
+          )
+          keybinds.remap_event( "norg", "i",
+            "<c-l>",
+            "core.integrations.telescope.insert_link"
+          )
+        end,
       },
     },
     ["core.export"] = { config = { extensions = "all" } },
@@ -53,33 +55,15 @@ require("neorg").setup({
       },
     },
     ["core.integrations.telescope"] = {},
-    ["core.norg.journal"] = {},
   },
 })
 
-local neorg_callbacks = require("neorg.callbacks")
-
-neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-  -- Map all the below keybinds only when the "norg" mode is active
-  keybinds.map_event_to_mode("norg", {
-    i = {
-      { "<C-l>", "core.integrations.telescope.insert_link" },
-      { "<C-f>", "core.integrations.telescope.insert_file_link" },
-    },
-  }, {
-    silent = true,
-    noremap = true,
-  })
-end)
-
 vim.api.nvim_create_user_command("GtdCapture", function(_)
-  require("neorg").org_file_entered(true, "silent=true")
-  vim.cmd("Neorg gtd capture")
+  require("neorg.modules.core.gtd.ui.capture_popup").public.show_capture_popup()
 end, { force = true })
 vim.api.nvim_set_keymap("n", "<leader>otc", "<CMD>GtdCapture<CR>", { noremap = true })
 
 vim.api.nvim_create_user_command("GtdView", function(_)
-  require("neorg").org_file_entered(true, "silent=true")
   vim.cmd("Neorg gtd views")
 end, { force = true })
 vim.api.nvim_set_keymap("n", "<leader>otv", "<CMD>GtdView<CR>", { noremap = true })
