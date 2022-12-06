6920a61ef85f013b978ba2408aa3a08f21a6e713
alex.tylor <atylor@gmail.com>
2022-12-06 10:00:00 +0000

    feat: additional escape sequance

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -131,6 +131,7 @@ set("v", "gy", '"+y', { desc = "yank to system clipboard" })
 set("n", "gy", 'gv"+y', { desc = "select last selection and yank to system clipboard" })
 
 set("i", "jj", "<ESC>", { desc = "Exit insert mode" })
+set("i", "JJ", "<ESC>", { desc = "Exit insert mode" })
 set("i", "kk", "<ESC>", { desc = "Exit insert mode" })
 
 set("i", "<PageUp>", "", { desc = "Disbale pagedown in insert mode, keep hitting by mistake" })
