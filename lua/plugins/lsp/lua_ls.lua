c07799d5af701eb24ba87c6c045437001fdeca51
alex.tylor <atylor@gmail.com>
2023-02-17 14:47:48 +0000

    chore: fix deprecation warning

--- lua/plugins/lsp/lua_ls.lua
+++ lua/plugins/lsp/lua_ls.lua
@@ -2,7 +2,7 @@ local common = require("plugins.lsp.common")
 require("neodev")
 
 local lspconfig = require("lspconfig")
-lspconfig.sumneko_lua.setup({
+lspconfig.lua_ls.setup({
   -- add any options here, or leave empty to use the default settings
   lspconfig = {
     on_attach = common.on_attach,
