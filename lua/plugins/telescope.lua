6e4fd3a86a3b17ab8463dd76f92036a5786d2a15
alex.tylor <atylor@gmail.com>
2023-04-11 16:58:20 +0100

    fix: telescope find project files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,13 @@ return {
       },
       {
         "<c-p>",
-        ':lua require("plugins.telescope").project_files()<cr>',
+        function()
+          local opts = {} -- define here if you want to define something
+          local ok = pcall(require("telescope.builtin").git_files, opts)
+          if not ok then
+            require("telescope.builtin").find_files(opts)
+          end
+       end,
         desc = "telescope open file, either git_files or fallback to CWD",
       },
       {
