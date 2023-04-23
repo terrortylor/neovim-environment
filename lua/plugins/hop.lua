ba5e7b7bd34287cf0375f957eee5514a8b6c5562
alex.tylor <atylor@gmail.com>
2023-04-23 22:30:23 +0100

    feat: some extra lazy loading for hop keymaps

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,17 +1,3 @@
-local hint_char1_and_then = function(and_then_func)
-  local hop = require("hop")
-  local jump_target = require("hop.jump_target")
-  return function()
-    local opts = hop.opts
-    local c = hop.get_input_pattern("Hop 1 char: ", 1)
-    local generator = jump_target.jump_targets_by_scanning_lines
-    hop.hint_with_callback(generator(jump_target.regex_by_case_searching(c, true, opts)), opts, function(jt)
-      hop.move_cursor_to(jt.window, jt.line + 1, jt.column - 1, opts.hint_offset)
-      and_then_func()
-    end)
-  end
-end
-
 -- remote change/delete text objects
 local default_text_objects =
   { "w", "W", "s", "p", "[", "]", "(", ")", "b", ">", "<", "t", "{", "}", "B", '"', "'", "`" }
@@ -21,17 +7,14 @@ local generated_keys = {}
 for _, v in ipairs(default_text_objects) do
   table.insert(generated_keys, {
     "cir" .. v,
-    hint_char1_and_then(function()
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("ci" .. v, true, false, true), "n", true)
-    end),
+    ":lua require('plugins.lazy_wrappers.lazy_hop').feedkeys_at_remote('ci" .. v .. "', 'n')<cr>",
     desc = "jump to and change in: " .. v,
   })
 
   table.insert(generated_keys, {
     "dir" .. v,
-    hint_char1_and_then(function()
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("di" .. v, true, false, true), "n", true)
-    end),
+    "dir" .. v,
+    ":lua require('plugins.lazy_wrappers.lazy_hop').feedkeys_at_remote('di" .. v .. "', 'n')<cr>",
     desc = "jump to and delete in: " .. v,
   })
 end
@@ -43,39 +26,37 @@ table.foreach({
   { "<leader>/", "<CMD>HopPattern<CR>", desc = "Search for patter and jump to instance" },
   {
     "<leader>jd",
-    hint_char1_and_then(function()
-      local bf = vim.b.local_hop_definition
-      if bf then
-        bf()
-      else
-        if #vim.lsp.buf_get_clients(0) > 0 then
-          require("telescope.builtin").lsp_definitions()
-        end
-      end
-    end),
+    ":lua require('plugins.lazy_wrappers.lazy_hop').hint_lsp_definition()<cr>",
     desc = "Hop to location and go to definition",
   },
-  { "<leader>jD", hint_char1_and_then(vim.lsp.buf.declaration) },
-  { "<leader>ji", hint_char1_and_then(require("telescope.builtin").lsp_implementations) },
-  { "<leader>jr", hint_char1_and_then(require("telescope.builtin").lsp_references) },
-  { "<leader>jk", hint_char1_and_then(vim.lsp.buf.type_definition) },
-  -- TODO je don't seem ot trigger the and_then_func
-  { "<leader>je", hint_char1_and_then(vim.diagnostic.open_float) },
+  {
+    "<leader>jD",
+    ":lua require('plugins.lazy_wrappers.lazy_hop').hint_lsp_definition()<cr>",
+    desc = "Hop to location and go to declaration",
+  },
+  {
+    "<leader>ji",
+    ":lua require('plugins.lazy_wrappers.lazy_hop').hint_lsp_implementations()<cr>",
+    desc = "Hop to location and go to implementations",
+  },
+  {
+    "<leader>jr",
+    ":lua require('plugins.lazy_wrappers.lazy_hop').hint_lsp_references()<cr>",
+    desc = "Hop to location and go to references",
+  },
+  {
+    "<leader>jk",
+    ":lua require('plugins.lazy_wrappers.lazy_hop').hint_lsp_type_definition()<cr>",
+    desc = "Hop to location and go to type declaration",
+  },
+  -- -- TODO je don't seem ot trigger the and_then_func
+  -- { "<leader>je", hint_char1_and_then(vim.diagnostic.open_float) },
   -- Playing with f/F replacement
   -- TODO come back to this, HopChar1 is basically all I need along wiht hint_char1_and_then
   { "<leader>jf", "<cmd>lua require'hop'.hint_char1({ current_line_only = true })<cr>" },
   {
     "cirq",
-    hint_char1_and_then(function()
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('ci"', true, false, true), "n", true)
-    end),
-    desc = 'jump to and change in: " alias',
-  },
-  {
-    "dirq",
-    hint_char1_and_then(function()
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('di"', true, false, true), "n", true)
-    end),
+    ":lua require('plugins.lazy_wrappers.lazy_hop').feedkeys_at_remote('ciq', 'n')<cr>",
     desc = 'jump to and change in: " alias',
   },
 }, function(_, v)
---
+++ lua/plugins/lazy_wrappers/lazy_hop.lua
@@ -0,0 +1,54 @@
+-- This is really just to implrove lazy loading...
+local M = {}
+
+function M.hint_char1_and_then(and_then_func)
+  local hop = require("hop")
+  local jump_target = require("hop.jump_target")
+  -- return function()
+  local opts = hop.opts
+  local c = hop.get_input_pattern("Hop 1 char: ", 1)
+  local generator = jump_target.jump_targets_by_scanning_lines
+  hop.hint_with_callback(generator(jump_target.regex_by_case_searching(c, true, opts)), opts, function(jt)
+    hop.move_cursor_to(jt.window, jt.line + 1, jt.column - 1, opts.hint_offset)
+    and_then_func()
+  end)
+  -- end
+end
+
+function M.feedkeys_at_remote(action, mode)
+  M.hint_char1_and_then(function()
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(action, true, false, true), mode, true)
+  end)
+end
+
+function M.hint_lsp_definition()
+  M.hint_char1_and_then(function()
+    local bf = vim.b.local_hop_definition
+    if bf then
+      bf()
+    else
+      if #vim.lsp.buf_get_clients(0) > 0 then
+        require("telescope.builtin").lsp_definitions()
+      end
+    end
+  end)
+end
+
+function M.hint_lsp_declaration()
+  M.hint_char1_and_then(vim.lsp.declaration)
+end
+
+function M.hint_lsp_implementations()
+  M.hint_char1_and_then(vim.lsp.implementations)
+end
+
+function M.hint_lsp_references()
+  M.hint_char1_and_then(vim.lsp.references)
+end
+
+function M.hint_lsp_type_definition()
+  M.hint_char1_and_then(vim.lsp.type_definition)
+end
+
+
+return M
