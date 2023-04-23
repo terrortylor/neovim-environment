ab967b1d2a7fe89f99e18bc926b4c046163fe92d
alex.tylor <atylor@gmail.com>
2023-04-23 22:32:00 +0100

    feat: add noover.nvim to play with

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -172,4 +172,30 @@ return {
       { "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Make it rain" },
     },
   },
+
+  {
+    "lewis6991/hover.nvim",
+    keys = {
+      { "KK", ':lua require("hover").hover()<cr>', desc = "hover.nvim" },
+      { "gKK", ':lua require("hover").hover_select()<cr>', desc = "hover.nvim (select)" },
+    },
+    opts = {
+      init = function()
+        -- Require providers
+        require("hover.providers.lsp")
+        -- require('hover.providers.gh')
+        -- require('hover.providers.gh_user')
+        -- require('hover.providers.jira')
+        -- require('hover.providers.man')
+        require('hover.providers.dictionary')
+      end,
+      preview_opts = {
+        border = nil,
+      },
+      -- Whether the contents of a currently open hover window should be moved
+      -- to a :h preview-window when pressing the hover keymap.
+      preview_window = false,
+      title = true,
+    },
+  },
 }
