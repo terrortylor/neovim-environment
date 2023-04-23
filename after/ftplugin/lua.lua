3bf92838537b3debeaf8c4b62dfea9f19fd5ed13
alex.tylor <atylor@gmail.com>
2023-04-23 21:36:31 +0100

    fix: make sure Telescope lazy load on cmd

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,5 +1,22 @@
 -- TODO change to vim.keymap.set? search for other occourances
-local function keymap(...) vim.api.nvim_buf_set_keymap(0, ...) end
-keymap("n", "<leader>gtf", "<cmd>lua require('plugins.vim-test').lua_test_file()<cr>", {})
-keymap("n", "<leader>gtt", "<cmd>lua require('plugins.vim-test').lua_test_last()<cr>", {})
+local function keymap(...)
+  vim.api.nvim_buf_set_keymap(0, ...)
+end
 
+-- helpers for plenary
+local lua_last_file = ""
+
+-- keymap("n", "<leader>gtf", function()
+--   lua_last_file = vim.fn.expand("%:p")
+--   vim.cmd("silent! wall")
+--   require("plenary.test_harness").test_directory(lua_last_file)
+-- end, {})
+
+-- keymap("n", "<leader>gtt", function()
+--   if lua_last_file == "" then
+--     print("No file tested yet!")
+--     return
+--   end
+--   vim.cmd("silent! wall")
+--   require("plenary.test_harness").test_directory(lua_last_file)
+-- end, {})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
     keys = {
       {
         "<leader>fr",
