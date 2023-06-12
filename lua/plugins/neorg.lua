1ad6577f0536b402bf064222e84644f3a55c263c
alex.tylor <atylor@gmail.com>
2023-06-12 21:19:56 +0100

    chore(neorg): tidy config, fix up new local leader as clashes with tmux leader

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -2,68 +2,68 @@ return {
   {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
-    ft = "norg",
+    depencencies = {
+      "nvim-neorg/neorg-telescope",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    keys = {
+      { "<c-g>nw", "<cmd>Neorg workspace<cr>", desc = "Neorg Workspace" },
+      { "<c-g>nn", "<cmd>Neorg keybind<cr>", desc = "Neorg New Note" },
+      { "<c-g>nr", "<cmd>Neorg return<cr>", desc = "Neorg Close & return" },
+    },
+    -- depencencies = { "~/personal-workspace/nvim-plugins/neorg-telescope" },
+    event = "BufReadPre,BufNew *.norg",
     opts = {
       load = {
-        ["core.defaults"] = {}, -- Load all the default modules
-        ["core.qol.toc"] = {}, -- table of contents module
-        ["core.keybinds"] = { -- Configure core.keybinds
-          config = {
-            default_keybinds = true,
-            neorg_leader = "<Leader>o", -- This is the default if unspecified
-            hook = function(keybinds)
-              -- luacheck: ignore
-              keybinds.remap_event("norg", "i", "<c-f>", "core.integrations.telescope.insert_file_link")
-              keybinds.remap_event("norg", "i", "<c-l>", "core.integrations.telescope.insert_link")
-              keybinds.unmap("norg", "n", "<,")
-              keybinds.unmap("norg", "n", ">.")
-              keybinds.remap_event("norg", "n", ">", "core.promo.promote")
-              keybinds.remap_event("norg", "n", "<", "core.promo.demote")
-            end,
-          },
-        },
-        ["core.export"] = {},
-        -- ["core.export"] = { config = { extensions = "all" } },
-        ["core.concealer"] = {
-          config = {
-            icons = {
-              marker = {
-                icon = "|",
-              },
-              todo = {
-                undone = { icon = " " },
-                pending = { icon = "P" },
-                done = { icon = "X" },
-                hold = { icon = "H" },
-                cancelled = { icon = "C" },
-              },
-            },
-          },
-        }, -- Allows for use of icons
-        ["core.dirman"] = { -- Manage your directories with Neorg
-          config = {
-            workspaces = {
-              my_workspace = "~/personal-workspace/notes",
-            },
-          },
-        },
         ["core.completion"] = {
           config = {
             engine = "nvim-cmp",
             name = "[Norg]",
           },
         },
+        ["core.concealer"] = {
+          config = {
+            icons = {
+              todo = {
+                undone = { icon = " " },
+              },
+            },
+          },
+        },
+        ["core.defaults"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/personal-workspace/notes",
+            },
+            default_workspace = "notes",
+          },
+        },
+        ["core.export"] = {},
+        ["core.keybinds"] = {
+          config = {
+            default_keybinds = true,
+            neorg_leader = "<c-g>",
+            hook = function(keybinds)
+              -- luacheck: ignore
+              keybinds.remap_event("norg", "i", "<c-f>", "core.integrations.telescope.insert_file_link")
+              keybinds.remap_event("norg", "i", "<c-l>", "core.integrations.telescope.insert_link")
+              keybinds.remap("norg", "n", "<space>ohl", "<cmd>Neorg toc qflist<CR>") -- toc / header list
+              keybinds.remap_event("norg", "n", "<c-g><c-g>", "core.qol.todo_items.todo.task_cycle") -- toc / header list
+            end,
+          },
+        },
+        ["core.qol.toc"] = {
+          config = {
+            close_after_use = true,
+          },
+        },
         ["core.syntax"] = {},
-        ["core.integrations.treesitter"] = {},
         ["core.integrations.telescope"] = {},
+        ["core.integrations.treesitter"] = {},
         ["core.integrations.nvim-cmp"] = {},
       },
     },
-    depencencies = {
-      "nvim-neorg/neorg-telescope",
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    -- depencencies = { "~/personal-workspace/nvim-plugins/neorg-telescope" },
   },
   { "nvim-neorg/neorg-telescope" },
 }
