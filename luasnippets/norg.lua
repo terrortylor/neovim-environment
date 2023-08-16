270a4aa17e0dd5c64679190ba123d51407ceacea
alex.tylor <atylor@gmail.com>
2023-08-16 23:53:17 +0100

    feat: nicer norg todo snip

--- luasnippets/norg.lua
+++ luasnippets/norg.lua
@@ -8,6 +8,7 @@ local d = ls.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
+local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
 local get_visual = function(args, parent)
   if #parent.snippet.env.LS_SELECT_RAW > 0 then
@@ -18,24 +19,32 @@ local get_visual = function(args, parent)
 end
 
 return {
+  s({ trig = "note", dscr = "Note with bold prefix" }, fmt("*NOTE*: {}", { i(1, "bash") })),
+
+  s({ trig = "todo", dscr = "Norg todo" }, fmt("- ( ) {}", { i(1, "bash") })),
+
   s(
-    { trig = "note", dscr = "Note with bold prefix" },
-    fmt(
-      [[
-      *NOTE*: {}
-    ]],
-      { i(1, "bash") }
-    )
+    {
+      trig = "^[%s]*(;+)tt",
+      descr = "Norg todo, with smart indenting",
+      regTrig = true,
+      wordTrig = false,
+      snippetType = "autosnippet",
+    },
+    fmt("{} ( ) {}", {
+      f(function(_, snip)
+        local len = snip.captures[1]:len()
+        return string.rep("-", len)
+      end),
+      d(1, get_visual),
+    })
   ),
 
   s(
-    { trig = "todo", dscr = "Norg todo" },
-    fmt(
-      [[
-      - ( ) {}
-    ]],
-      { i(1, "bash") }
-    )
+    { trig = ";bb", descr = "Norg bold text", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
+    fmt("*{}* ", {
+      d(1, get_visual),
+    })
   ),
 
   s(
@@ -52,29 +61,17 @@ return {
 
   s(
     { trig = "link", dscr = "Norg link" },
-    fmt(
-      [[
-      {<>}[<>]
-    ]],
-      {
-        d(1, get_visual),
-        i(2, "description"),
-      },
-      { delimiters = "<>" }
-    )
+    fmt("{<>}[<>]", {
+      d(1, get_visual),
+      i(2, "description"),
+    }, { delimiters = "<>" })
   ),
 
   s(
     { trig = "ivaas" },
-    fmt(
-      [[
-      {https://orc-id.atlassian.net/browse/IVAAS-<>}[IVAAS-<>] 
-    ]],
-      {
-        d(1, get_visual),
-        rep(1),
-      },
-      { delimiters = "<>" }
-    )
+    fmt("{https://orc-id.atlassian.net/browse/IVAAS-<>}[IVAAS-<>] ", {
+      d(1, get_visual),
+      rep(1),
+    }, { delimiters = "<>" })
   ),
 }
