cd0d92ca054c9a1e82525a1defa4f85ea69bb20d
alex.tylor <atylor@gmail.com>
2023-09-11 09:37:15 +0100

    feat(neorg): config with new note mappings, and some snippets

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -5,12 +5,36 @@ return {
     depencencies = {
       "nvim-neorg/neorg-telescope",
       "nvim-treesitter/nvim-treesitter-textobjects",
-      { "pysan3/neorg-templates", dependencies = { "L3MON4D3/LuaSnip" } },
+      -- { "pysan3/neorg-templates", dependencies = { "L3MON4D3/LuaSnip" } },
+    },
+    cmd = {
+      "Neorg",
     },
     keys = {
       { "<c-g>nw", "<cmd>Neorg workspace<cr>", desc = "Neorg Workspace" },
-      { "<c-g>nn", "<cmd>Neorg keybind<cr>", desc = "Neorg New Note" },
+      { "<c-g>nn", function() -- create a new slipbox note
+        -- TODO popup to offer adding link also to where ever I am?
+        require("neorg.modules.core.dirman.module").public.create_file("slipbox/" .. os.date("%Y%m%d%H%M%S"), "notes", {no_open = false, force = false})
+        local snips = require("luasnip").get_snippets()[vim.bo.ft]
+        for _, snip in ipairs(snips) do
+          if snip["name"] == "templateslipbox" then
+            require("luasnip").snip_expand(snip)
+          end
+        end
+      end, desc = "Neorg New Slip Note" },
       { "<c-g>nr", "<cmd>Neorg return<cr>", desc = "Neorg Close & return" },
+      {
+        "<c-g>nd",
+        function()
+          vim.cmd([[
+          Neorg journal yesterday
+          topleft vsplit
+          Neorg journal today
+          Neorg templates fload journal
+          ]])
+        end,
+        desc = "Neorg ",
+      },
     },
     -- depencencies = { "~/personal-workspace/nvim-plugins/neorg-telescope" },
     event = "BufReadPre,BufNew *.norg",
@@ -41,6 +65,7 @@ return {
           },
         },
         ["core.export"] = {},
+        ["core.itero"] = {},
         ["core.journal"] = {},
         ["core.keybinds"] = {
           config = {
@@ -52,26 +77,23 @@ return {
               keybinds.remap_event("norg", "i", "<c-l>", "core.integrations.telescope.insert_link")
               keybinds.remap("norg", "n", "<space>ohl", "<cmd>Neorg toc qflist<CR>") -- toc / header list
               keybinds.remap_event("norg", "n", "<c-g><c-g>", "core.qol.todo_items.todo.task_cycle") -- toc / header list
+              keybinds.unmap("norg", "n", "<c-g>nn")
             end,
           },
         },
+        ["core.promo"] = {},
         ["core.qol.toc"] = {
           config = {
             close_after_use = true,
           },
         },
         ["core.syntax"] = {},
+        ["core.upgrade"] = {},
         ["core.integrations.telescope"] = {},
         ["core.integrations.treesitter"] = {},
         ["core.integrations.nvim-cmp"] = {},
-        ["external.templates"] = {
-          config = {
-            templates_dir = vim.fn.stdpath("config") .. "/templates/neorg",
-          },
-        },
       },
     },
   },
   { "nvim-neorg/neorg-telescope" },
-  { "pysan3/neorg-templates" },
 }
--- luasnippets/norg.lua
+++ luasnippets/norg.lua
@@ -10,12 +10,34 @@ local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
-local get_visual = function(args, parent)
+local function get_visual(args, parent)
   if #parent.snippet.env.LS_SELECT_RAW > 0 then
     return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
   else
     return sn(nil, i(1))
   end
+end
+
+local function get_timezone_offset()
+  -- http://lua-users.org/wiki/TimeZon
+  -- return the timezone offset in seconds, as it was on the time given by ts
+  -- Eric Feliksik
+  local utcdate = os.date("!*t", 0)
+  local localdate = os.date("*t", 0)
+  localdate.isdst = false -- this is the trick
+  return os.difftime(os.time(localdate), os.time(utcdate))
+end
+
+local function get_timestamp()
+  -- generate a ISO-8601 timestamp
+  -- example: 2023-09-05T09:09:11-0500
+  local tz_offset = get_timezone_offset()
+  local h, m = math.modf(tz_offset / 3600)
+  return os.date("%Y-%m-%dT%H:%M:%S") .. string.format("%+.4d", h * 100 + m * 60)
+end
+
+local function neorg_meta_version()
+  return require("neorg.core").config.norg_version
 end
 
 return {
@@ -48,7 +70,7 @@ return {
   ),
 
   s(
-    { trig = "code", dscr = "Norg code block" },
+    { trig = "^[%s]*;cc", descr = "Norg code block", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
     fmt(
       [[
       @code {}
@@ -60,7 +82,7 @@ return {
   ),
 
   s(
-    { trig = "link", dscr = "Norg link" },
+    { trig = ";ll", descr = "Norg link", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
     fmt("{<>}[<>]", {
       d(1, get_visual),
       i(2, "description"),
@@ -73,5 +95,109 @@ return {
       d(1, get_visual),
       rep(1),
     }, { delimiters = "<>" })
+  ),
+
+  s(
+    { trig = "templatejournal", descr = "Template for Journal entry", regTrig = false },
+    fmt(
+      [[
+      @document.meta
+      title: {}
+      description: 
+      authors: alex.tylor
+      categories: journal
+      created: {}
+      updated: {}
+      version: {}
+      @end
+
+      * Tasks
+      {}
+
+      * Planner
+
+        - ( ) 0900 - 
+        - ( ) 1000 - 
+        - ( ) 1100 - 
+        - ( ) 1200 - 
+        - ( ) 1300 - 
+        - ( ) 1400 - 
+        - ( ) 1500 - 
+        - ( ) 1600 - 
+        - ( ) 1700 - 
+        - ( ) 1800 - 
+
+      * Notes
+    ]],
+      {
+        i(1, "Title"),
+        f(get_timestamp, {}),
+        f(get_timestamp, {}),
+        f(neorg_meta_version, {}),
+        i(2, ""),
+      }
+    )
+  ),
+
+  s(
+    { trig = "templateproject", descr = "Template for project", regTrig = false },
+    fmt(
+      [[
+      @document.meta
+      title: {}
+      description: {}
+      authors: alex.tylor
+      categories: {}
+      created: {}
+      updated: {}
+      version: {}
+      @end
+
+      {}
+
+    ]],
+      {
+        i(1, "Title"),
+        i(2, ""),
+        i(3, ""),
+        f(get_timestamp, {}),
+        f(get_timestamp, {}),
+        f(neorg_meta_version, {}),
+        i(4, ""),
+      }
+    )
+  ),
+
+  s(
+    { trig = "templateslipbox", descr = "Template for slipbox entry", regTrig = false },
+    fmt(
+      [[
+      @document.meta
+      title: {}
+      description: {}
+      authors: alex.tylor
+      categories: {}
+      created: {}
+      updated: {}
+      version: {}
+      @end
+
+      * Overview
+      {}
+
+      * Notes
+
+      * Resources
+    ]],
+      {
+        i(1, "Title"),
+        i(2, ""),
+        i(3, ""),
+        f(get_timestamp, {}),
+        f(get_timestamp, {}),
+        f(neorg_meta_version, {}),
+        i(4, ""),
+      }
+    )
   ),
 }
