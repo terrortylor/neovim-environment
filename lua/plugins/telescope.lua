fc44546cc01b3035e9d51ba7b75103c41ce21492
alex.tylor <atylor@gmail.com>
2023-10-30 09:48:31 +0000

    feat(easypick): Add telescope easypick plugin to add some useful git pickers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -95,4 +95,27 @@ return {
     "stevearc/dressing.nvim",
     opts = {},
   },
+  {
+    "axkirillov/easypick.nvim",
+    config = function()
+      local easypick = require("easypick")
+      easypick.setup({
+        pickers = {
+          -- diff current branch with base_branch and show files that changed with respective diffs in preview
+          {
+            name = "changed_files",
+            command = "git diff --name-only",
+            previewer = easypick.previewers.branch_diff(),
+          },
+
+          -- list files that have conflicts with diffs in preview
+          {
+            name = "conflicts",
+            command = "git diff --name-only --diff-filter=U --relative",
+            previewer = easypick.previewers.file_diff(),
+          },
+        },
+      })
+    end,
+  },
 }
