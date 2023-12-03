db6824c00c73286e20b96add1d2d06a21423791d
alex.tylor <atylor@gmail.com>
2023-12-03 21:45:10 +0000

    chore: remove old plugin config, not used for long enough can't be missing anything

--- lua/pluginsold/lsp/terraform.lua
+++
@@ -1,9 +0,0 @@
-local common = require("plugins.lsp.common")
-
-require("util.health").register_required_binary("terraform-ls", "Reqiured for terraform LSP config")
--- https://github.com/hashicorp/terraform-ls/blob/main/docs/installation.md
-
-require("lspconfig").terraformls.setup({
-  on_attach = common.on_attach,
-  capabilities = common.buildCapabilities(),
-})
--- lua/pluginsold/lsp/tsserver.lua
+++
@@ -1,7 +0,0 @@
-local common = require("plugins.lsp.common")
-require("util.health").register_required_binary ("typescript-language-server", "Used for JS/TSX LSP")
-
-require("lspconfig").tsserver.setup({
-  on_attach = common.on_attach,
-  capabilities = common.buildCapabilities(),
-})
