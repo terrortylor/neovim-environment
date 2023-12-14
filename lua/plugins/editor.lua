7fa55153726cc35c363bc660a5340a96cccf74a1
alex.tylor <atylor@gmail.com>
2023-12-14 14:33:15 +0000

    chore(replacer.nvim): disable auto save and add mappinng to save

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -41,8 +41,10 @@ return {
 
   {
     "gabrielpoca/replacer.nvim",
+    opts = { rename_files = false },
     keys = {
-      { "<leader>h", ':lua require("replacer").run()<cr>' },
+      { "<leader>h", ':lua require("replacer").run()<cr>', desc = "quickfix: run replacer.nvim" },
+      { "<leader>H", ':lua require("replacer").save()<cr>', desc = "quickfix: save replacer.nvim" },
     },
   },
 
