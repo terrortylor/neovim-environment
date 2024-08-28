01d707c9a77aac4280b30df5676f9b02969346ce
alex.tylor <atylor@gmail.com>
2024-08-28 22:12:24 +0100

    chore(markdown): modify markview.nvim checkbox symbols

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -15,20 +15,36 @@ return {
 
       markview.setup({
         headings = presets.headings.glow_labels,
+        checkboxes = {
+          enable = true,
+
+          checked = {
+            text = "[✔]",
+            hl = "MarkviewCheckboxChecked",
+          },
+          pending = {
+            text = "[◯]",
+            hl = "MarkviewCheckboxPending",
+          },
+          unchecked = {
+            text = "[✘]",
+            hl = "MarkviewCheckboxUnchecked",
+          },
+        },
       })
-      
+
       -- vim.cmd("Markview enableAll")
     end,
   },
 
-  {
-    "jakewvincent/mkdnflow.nvim",
-    config = function()
-      require("mkdnflow").setup({
-        -- Config goes here; leave blank for defaults
-      })
-    end,
-  },
+  -- {
+  --   "jakewvincent/mkdnflow.nvim",
+  --   config = function()
+  --     require("mkdnflow").setup({
+  --       -- Config goes here; leave blank for defaults
+  --     })
+  --   end,
+  -- },
 
   {
     "iamcco/markdown-preview.nvim",
@@ -38,5 +54,4 @@ return {
       vim.fn["mkdp#util#install"]()
     end,
   },
-
 }
