9462058472f4efe1294b890edf408a080f45d8b8
alex.tylor <atylor@gmail.com>
2024-08-29 11:51:28 +0100

    chore(lazyload): optimise loading colourscheme, these seemed to half to time!

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -6,7 +6,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 
