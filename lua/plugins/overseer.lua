93fa9c44ae484b5f4f9b429a0e91a95f4c7f4469
alex.tylor <atylor@gmail.com>
2024-09-27 13:52:57 +0100

    chore(overseer): tidy up from testing out

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -11,6 +11,7 @@ return {
     },
     cmd = {
       "OverseerRun",
+      "OverseerRunAndOpen",
       "OverseerInfo",
       "OverseerOpen",
       "OverseerBuild",
@@ -56,29 +57,6 @@ return {
       end, {})
 
       overseer.register_template({
-        name = "Helm Generate Golden Files",
-        builder = function()
-          -- local file = vim.fn.expand("%:p")
-          -- cmd = { "sh", file }
-          cmd = { "make", "generate-golden-environment-manifest" }
-          return {
-            cmd = cmd,
-            components = {
-              -- --   { "on_output_quickfix", set_diagnostics = true },
-              { "open_output", direction = "vertical", focus = false, on_start = "always" },
-              --   { "on_output_write_file", filename = '/tmp/ggg'},
-              -- --   "on_result_diagnostics",
-              -- --   "default",
-            },
-          }
-        end,
-        condition = {
-          -- filetype = { "sh", "bash" },
-          dir = "~/workspace/platform-provision",
-        },
-      })
-
-      overseer.register_template({
         name = "Run Shell Script",
         builder = function()
           local file = vim.fn.expand("%:p")
@@ -99,17 +77,10 @@ return {
         },
       })
 
-      vim.api.nvim_create_user_command("RunAndOpen", function()
-        local overseer = require("overseer")
-        overseer.run_template({ name = "Run Shell Script" }, function(task)
+      vim.api.nvim_create_user_command("OverseerRunAndOpen", function()
+        overseer.run_template({}, function(task)
           if task then
-            -- task:add_component({"open_output", direction = "vertical", focus=false, on_start="always"})
-            -- task:add_component({ "restart_on_save", paths = {vim.fn.expand("%:p")} })
-            -- local main_win = vim.api.nvim_get_current_win()
-            overseer.run_action(task, "open vsplit")
-          -- vim.api.nvim_set_current_win(main_win)
-          else
-            vim.notify("WatchRun not supported for filetype " .. vim.bo.filetype, vim.log.levels.ERROR)
+            overseer.open({ enter = false, direction = "bottom" })
           end
         end)
       end, {})
