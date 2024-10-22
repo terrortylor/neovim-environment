3cbef237d7e8ffb59b541b66e25dd8c17899419b
alex.tylor <atylor@gmail.com>
2024-10-22 22:15:29 +0100

    feat(completion): add source to cmp pop-up, some tuning around spell/buffer/tmux suggestions

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -52,6 +52,12 @@ return {
             end
           end,
         },
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = entry.source.name
+            return vim_item
+          end,
+        },
         mapping = {
           -- ["<C-k>"] = cmp.mapping.select_prev_item(),
           -- ["<C-j>"] = cmp.mapping.select_next_item(),
@@ -113,43 +119,48 @@ return {
           max_item_count = 5,
           priorty = 1000,
         },
-        { name = "spell", keyword_length = 5, max_item_count = 3 },
-        {
-          name = "buffer",
-          keyword_length = 4,
-          max_item_count = 5,
-          priorty = 1000,
+        { name = "spell", keyword_length = 4, max_item_count = 5,
+        option = {
+          keep_all_entries = false,
+          preselect_correct_word = true,
         },
-      }),
-    })
-
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
       },
-    })
+      {
+        name = "buffer",
+        keyword_length = 4,
+        max_item_count = 5,
+        priorty = 1000,
+      },
+    }),
+  })
 
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-        { name = "cmdline" },
-      }),
-    })
-  end,
+  cmp.setup.cmdline({ "/", "?" }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "buffer" },
+    },
+  })
 
-  dependencies = {
-    "L3MON4D3/LuaSnip",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lsp-document-symbol",
-    "hrsh7th/cmp-path",
-    "saadparwaiz1/cmp_luasnip",
-    "andersevenrud/cmp-tmux",
-    "f3fora/cmp-spell",
-    "hrsh7th/cmp-nvim-lua",
-  },
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "path" },
+      { name = "cmdline" },
+    }),
+  })
+end,
+
+dependencies = {
+  "L3MON4D3/LuaSnip",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-nvim-lsp-document-symbol",
+  "hrsh7th/cmp-path",
+  "saadparwaiz1/cmp_luasnip",
+  "andersevenrud/cmp-tmux",
+  "f3fora/cmp-spell",
+  "hrsh7th/cmp-nvim-lua",
+},
 },
 }
