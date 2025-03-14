d5b0cf675bea060902897f95ff1e4a65945d98ca
alex.tylor <atylor@gmail.com>
2025-03-14 12:14:40 +0000

    chore: update note filetype detect string

--- after/ftdetect/note.lua
+++ after/ftdetect/note.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-  pattern = "*/personal-workspace/notes/*/*.md",
+  pattern = "*/personal-workspace/notes/*.md",
   callback = function()
     vim.bo.filetype = "markdown.note"
   end
